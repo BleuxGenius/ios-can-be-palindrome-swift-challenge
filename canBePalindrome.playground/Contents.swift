ab19e9cc5da317f670b5cad4655e9eaff7a6cfd4
Dani Blackwell <bleuxgenius@gmail.com>
2019-09-18 10:51:44 -0500

    Completed

--- canBePalindrome.playground/Contents.swift
+++ canBePalindrome.playground/Contents.swift
@@ -1,7 +1,18 @@
 import Foundation
 
 func canBePalindrome(_ string: String) -> Bool {
-
+    
+//    create new instant. according to parameters
+    let reversedString = String(string.reversed())
+//    if the string is "!=" <- not equal to empty string AND the same string is equal to reverse string
+    if (string != "" && string == reversedString) {
+//        return that its true
+        return true
+//        if it does not
+    } else {
+//        return its false 
+        return false
+    }
 }
 
 
